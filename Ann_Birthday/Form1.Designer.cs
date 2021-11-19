5e6b7c7ecfb30a2863487f67140933b9eacfc948
suryakumar-exe <suryakumar2808@gmail.com>
2021-11-19 14:18:20 +0530

    first commit

---
+++ Ann_Birthday/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Ann_Birthday
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.lbl_record = new System.Windows.Forms.Label();
+            this.testdate = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.send = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSearch.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSearch.Location = new System.Drawing.Point(713, 23);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(101, 31);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(58, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search By :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(898, 473);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btn_open
+            // 
+            this.btn_open.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_open.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_open.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_open.Location = new System.Drawing.Point(858, 577);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(96, 33);
+            this.btn_open.TabIndex = 4;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = false;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // lbl_record
+            // 
+            this.lbl_record.AutoSize = true;
+            this.lbl_record.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_record.ForeColor = System.Drawing.Color.Blue;
+            this.lbl_record.Location = new System.Drawing.Point(53, 577);
+            this.lbl_record.Name = "lbl_record";
+            this.lbl_record.Size = new System.Drawing.Size(142, 21);
+            this.lbl_record.TabIndex = 5;
+            this.lbl_record.Text = "Search  record:";
+            this.lbl_record.Click += new System.EventHandler(this.lbl_record_Click);
+            // 
+            // testdate
+            // 
+            this.testdate.AutoSize = true;
+            this.testdate.Location = new System.Drawing.Point(964, 54);
+            this.testdate.Name = "testdate";
+            this.testdate.Size = new System.Drawing.Size(0, 13);
+            this.testdate.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(364, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(324, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Birthday",
+            "Anniversaryday"});
+            this.comboBox1.Location = new System.Drawing.Point(222, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 25);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // send
+            // 
+            this.send.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.send.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold);
+            this.send.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.send.Location = new System.Drawing.Point(985, 72);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(103, 37);
+            this.send.TabIndex = 12;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = false;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(741, 577);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 33);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(Ann_Birthday.Form1);
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(Ann_Birthday.Program);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 701);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.testdate);
+            this.Controls.Add(this.lbl_record);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Label lbl_record;
+        private System.Windows.Forms.Label testdate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.BindingSource programBindingSource;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ann_Birthday/Form1.cs
@@ -0,0 +1,217 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace Ann_Birthday
+{
+    public partial class Form1 : Form
+    {
+        NetworkCredential login;
+        SmtpClient client;
+        MailMessage msg;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter="Excel Workbook|*.xlsx",Multiselect=false})
+            {
+                if(ofd.ShowDialog()==DialogResult.OK)
+                {
+
+                    //Cursor.Current = Cursor.WaitCursor;
+                    Cursor.Current = Cursors.WaitCursor;
+                    DataTable dt = new DataTable();
+                    using(XLWorkbook workbook = new XLWorkbook(ofd.FileName) )
+                    {
+                        bool isFirstRow = true;
+                        var rows = workbook.Worksheet(1).RowsUsed();
+                        foreach(var row in rows)
+                        {
+                            if (isFirstRow)
+                            {
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Columns.Add(cell.Value.ToString());
+                                isFirstRow = false;
+                            }
+                            else
+                            {
+                                dt.Rows.Add();
+                                int i = 0;
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Rows[dt.Rows.Count - 1][i++] = cell.Value.ToString();
+
+                            }
+                        }
+                        dataGridView1.DataSource = dt.DefaultView;
+                       
+                        lbl_record.Text = $"Total Records :{(dataGridView1.RowCount)-1}";
+                        Cursor.Current = Cursors.Default;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Birthday")
+            {
+                try
+                {
+
+                    DataView dv = dataGridView1.DataSource as DataView;
+                    if (dv != null)
+                    {
+
+                        //dv.RowFilter = txtSearch.Text;
+                        String date = dateTimePicker1.Text.Substring(0, 5);
+                        dv.RowFilter = "Birthday like'" + date + "%'";
+                        lbl_record.Text = $"Total Records :{(dataGridView1.RowCount) - 1}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if(comboBox1.Text == "Anniversaryday")
+            {
+                try
+                {
+
+                    DataView dv = dataGridView1.DataSource as DataView;
+                    if (dv != null)
+                    {
+
+                        //dv.RowFilter = txtSearch.Text;
+                        String date = dateTimePicker1.Text.Substring(0,5);
+                        dv.RowFilter = "Anniversaryday like'" + date + "%'";
+                        lbl_record.Text = $"Total Records :{(dataGridView1.RowCount) - 1}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                btnSearch.PerformClick();
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+
+            for (int r = 0; r < (dataGridView1.RowCount - 1); r++)
+            {
+                login = new NetworkCredential("suryafandasydream11@gmail.com", "Surya@123");
+                client = new SmtpClient("smtp.gmail.com");
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+
+                client.Credentials = login;
+
+                msg = new MailMessage { From = new MailAddress("suryafandasydream11@gmail.com", "Surya", Encoding.UTF8) };
+
+                String addr = Convert.ToString(dataGridView1.Rows[r].Cells["email"].Value);
+                //MessageBox.Show(addr);
+                msg.To.Add(new MailAddress(addr));
+                if (!string.IsNullOrEmpty(""))
+                {
+                    msg.To.Add(new MailAddress(""));
+                }
+                msg.Subject = "BIRTHDAY MAIL";
+                msg.Body = "Wishes you all NORDEX TESTING :)";
+                msg.BodyEncoding = Encoding.UTF8;
+                msg.IsBodyHtml = true;
+                msg.Priority = MailPriority.Normal;
+
+                msg.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+                string userstate = "Sending...";
+                client.SendAsync(msg, userstate);
+               //MessageBox.Show((dataGridView1.RowCount - 1));
+                if (r.Equals(dataGridView1.RowCount - 2)) {
+                    client.SendCompleted += new SendCompletedEventHandler(SendCompleteCallback);
+                }
+               
+            }
+
+
+
+
+        }
+        private static void SendCompleteCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+                MessageBox.Show(string.Format("{0} send cancelled.", e.UserState), "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (e.Error != null)
+                MessageBox.Show(string.Format("{0} {1}", e.UserState, e.Error), "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Your message sended successfully.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void lbl_record_Click(object sender, EventArgs e)
+         {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
